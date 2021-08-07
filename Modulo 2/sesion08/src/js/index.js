d73bd1c5f434170b4ee7dfee17c407b39824fbb4
mnchava <mnchava@outlook.com>
2021-08-06 21:57:23 -0500

    ignore

--- Modulo 2/sesion08/src/js/index.js
+++ Modulo 2/sesion08/src/js/index.js
@@ -1,2 +1,10 @@
 
 import "/src/styles.css";
+
+// Babel Input: ES6 arrow function
+[1, 2, 3].map((n) => n + 1);
+
+// Babel Output: ES5 equivalent
+[1, 2, 3].map(function(n) {
+  return n + 1;
+});
